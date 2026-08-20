eeb3ca9f7d7d66ccda40c5b8de27ac2be2686203
Bruce Wells <brucekwells@gmail.com>
2026-08-20 11:43:22 -0400

    Exclude new PDO directory due to bad symfony polly fills

--- Tests/SyntaxTest.php
+++ Tests/SyntaxTest.php
@@ -4,6 +4,7 @@ class SyntaxTest extends \PHPFUI\PHPUnitSyntaxCoverage\Extensions
 	{
 	public function testDirectory() : void
 		{
+		$this->addSkipDirectory('/PHPFUI/ORM/PDO');
 		$this->assertValidPHPDirectory(__DIR__ . '/../src', 'src directory has an error');
 		$this->assertValidPHPDirectory(__DIR__ . '/../Tests', 'Tests directory has an error');
 		}
